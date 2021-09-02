27111fbc43e57ca078f0c5c677547fe15d0d25e6
RahatMinhaz <minhaz7591@gmail.com>
2021-09-02 18:24:27 +0600

    first commit

---
+++ js/books.js
@@ -0,0 +1,31 @@
+const searchBook = () => {
+    const searchField =document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = '';
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResults(data.docs));
+}
+
+const displaySearchResults = books => {
+    const searchResults = document.getElementById('search-result');
+    books.forEach(book => {
+        console.log(book);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class = "card-title">${book.title}</h5>
+                <h5>${book.author_name}</h5>
+                <h5>Published Year: ${book.first_publish_year}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        searchResults.appendChild(div);
+    })
+}
